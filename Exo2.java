bb48a079238f312fde86d24152e04a175aa0720a
Younes Azirgui <163010503+younss95@users.noreply.github.com>
2026-09-22 16:39:09 +0200

    Modif des commentaires

--- Exo2.java
+++ Exo2.java
@@ -2,10 +2,6 @@ package exo2;
 
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
 /**
  *
